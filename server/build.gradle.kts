7eafad1c3fe1266b8b3ccea47bcbc2215c46a272
Robert Eric Reeves <robertericreeves@gmail.com>
2025-10-20 11:58:39 -0500

    Update remote-sdk to 1.1.0

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib"))
-	implementation("com.datadatdat:remote-sdk:1.0.0")
+	implementation("com.datadatdat:remote-sdk:1.1.0")
 	implementation("com.datadatdat:command-executor:1.0.0")
 	implementation("com.google.code.gson:gson:2.13.2")
 	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
