6db2aa7eee83337ada14d3ccb395dabe804f0fb2
Robert Eric Reeves <robertericreeves@gmail.com>
2025-11-11 15:13:29 -0600

    Update dependencies to remote-sdk:1.4.0 and command-executor:1.3.0

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib"))
-	implementation("com.datadatdat:remote-sdk:1.3.0")
+	implementation("com.datadatdat:remote-sdk:1.4.0")
 	implementation("com.datadatdat:command-executor:1.3.0")
 	implementation("com.google.code.gson:gson:2.13.2")
 	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
