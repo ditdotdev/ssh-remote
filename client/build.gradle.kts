7e844fa700dda49bbab0273b7f29dcf8c60b08db
Robert Eric Reeves <robertericreeves@gmail.com>
2025-11-19 10:31:13 -0600

    Update dependencies to remote-sdk:1.5.0 and command-executor:1.5.0

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.datadatdat:remote-sdk:1.4.0")
+    implementation("com.datadatdat:remote-sdk:1.5.0")
     testImplementation("io.mockk:mockk:1.14.5")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 }
