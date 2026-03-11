75f64e18820e6c7fa4f3fa100daa4b8e4551e6e2
Robert Eric Reeves <robertericreeves@gmail.com>
2026-03-11 14:57:03 -0500

    Fix command-executor version to 1.7.0 (unchanged in 1.7.1)

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation(kotlin("stdlib"))
 	implementation(kotlin("reflect"))
 	implementation("com.datadatdat:remote-sdk:1.7.1")
-	implementation("com.datadatdat:command-executor:1.7.1")
+	implementation("com.datadatdat:command-executor:1.7.0")
 	implementation("com.google.code.gson:gson:2.13.2")
 	testImplementation("io.kotest:kotest-runner-junit5:6.1.6")
 	testImplementation("io.kotest:kotest-assertions-core:6.1.6")
