bb70ac6345eda63bb382f88cf8498e6652b42d3a
Robert Eric Reeves <robertericreeves@gmail.com>
2026-05-06 12:17:15 -0500

    Update dependencies to 1.9.0

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib"))
 	implementation(kotlin("reflect"))
-	implementation("com.datadatdat:remote-sdk:1.8.7")
+	implementation("com.datadatdat:remote-sdk:1.9.0")
 	implementation("com.datadatdat:command-executor:1.9.0")
 	implementation("com.google.code.gson:gson:2.14.0")
 	testImplementation("io.kotest:kotest-runner-junit5:6.1.11")
