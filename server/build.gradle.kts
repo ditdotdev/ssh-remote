207df857819626ef7bd4d914b0e364180c5294af
Robert Eric Reeves <robertericreeves@gmail.com>
2026-06-15 11:25:04 -0500

    Update command-executor to 1.9.7

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation(kotlin("stdlib"))
 	implementation(kotlin("reflect"))
 	implementation("dev.dit:remote-sdk:1.9.6")
-	implementation("dev.dit:command-executor:1.9.6")
+	implementation("dev.dit:command-executor:1.9.7")
 	implementation("com.google.code.gson:gson:2.14.0")
 	testImplementation("io.kotest:kotest-runner-junit5:6.1.11")
 	testImplementation("io.kotest:kotest-assertions-core:6.1.11")
